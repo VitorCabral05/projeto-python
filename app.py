27c30b593ed4fc35300f102b9fd4d0dce548b5ef
VitorCabral05 <127370374+VitorCabral05@users.noreply.github.com>
2023-03-09 21:10:14 -0300

    Add files via upload
    Programa de inverter string

---
+++ app.py
@@ -0,0 +1,9 @@
+txt = "Este texto vai ser invertido"
+
+print(txt[::-1])
+
+def inverter(texto):  
+    return txt[::-1]
+
+print(inverter("texto"))
+print(inverter("Este tambem vai ser invertido"))
